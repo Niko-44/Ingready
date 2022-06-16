504bae2a5c596607deb8c573c80f2caf7cba2592
Niko-44 <106493627+Niko-44@users.noreply.github.com>
2022-06-16 19:15:12 -0300

    Paginas

--- Ingready/Ingready/Paginas/Usuario.xaml.cs
+++ Ingready/Ingready/Paginas/Usuario.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ingready.Vista;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +16,7 @@ namespace Ingready
         public Usuario()
         {
             InitializeComponent();
+            BindingContext = new verProductos();
         }
     }
 }
