10a0ca27c3add5e598b09f72d83178da11c35580
Niko-44 <106493627+Niko-44@users.noreply.github.com>
2022-07-05 22:30:15 -0300

    Se agrego el center and expand en las  paginas filtro y usuario

--- Ingready/Ingready/Paginas/Inicio.xaml.cs
+++ Ingready/Ingready/Paginas/Inicio.xaml.cs
@@ -14,5 +14,6 @@ namespace Ingready
         {
             InitializeComponent();
         }
+
     }
 }
--- Ingready/Ingready/Paginas/Usuario.xaml.cs
+++ Ingready/Ingready/Paginas/Usuario.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +16,28 @@ namespace Ingready
         public Usuario()
         {
             InitializeComponent();
+        }
+
+        string host = "ec2-54-147-33-38.compute-1.amazonaws.com";
+        string port = "5432";
+        string user = "vdppsccsqxqemn";
+        string database = "d25f8kt0526tsd";
+        string password = "8363ac24e726dd56e7e362f63a6b44c3789822b89fc7d174fe0d8e53bf17cca3";
+
+        private void btnCrear_Clicked(object sender, EventArgs e)
+        {
+            NpgsqlConnection conn = new NpgsqlConnection($"Server={host}; Port={port}; User Id={user};Password = {password}; Database={database};Pooling=true;Use SSL Stream=True;SSL Mode=Require;TrustServerCertificate=True;");
+
+            try
+            {
+                conn.Open();
+                DisplayAlert("Servidor", "Conectado correctamente", "Ok");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
         }
     }
 }
